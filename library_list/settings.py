4e54d47ff2f94ba8f7c54e6c007fb3d4f80cdaeb
suleecao <huiyingcao2@gmail.com>
2025-05-02 21:07:27 -0700

    first model, migration, env gitignore

--- library_list/settings.py
+++ library_list/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=ldpprp_%z0sl%kg$+k&o8#7&bs4j(=1ks%uj2$&c=_fv$)&33'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -75,10 +74,11 @@ WSGI_APPLICATION = 'library_list.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'librarylistsicle',
     }
 }
+
 
 
 # Password validation
